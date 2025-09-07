865d100131d5925b3aca2e9d68ee0237c6bac082
Cosmic-G-G <140998316+Cosmic-G-G@users.noreply.github.com>
2025-09-07 11:22:51 -0600

    going insane

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   module: {
@@ -12,10 +11,4 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "index.html"),
-      filename: 'index.html',
-    }),
-  ],
 };
