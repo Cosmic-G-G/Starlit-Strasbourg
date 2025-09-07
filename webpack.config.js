037cff90cc8ebd8bd3451094010be82fad396ede
Cosmic-G-G <140998316+Cosmic-G-G@users.noreply.github.com>
2025-09-07 10:32:19 -0600

    restoring

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const HTMLWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   module: {
@@ -12,10 +11,4 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
-  plugins: [
-    new HTMLWebpackPlugin({
-      template: 'index.html',
-      filename: 'index.html',
-    }),
-  ]
 };
